723e087b4904f1a8eb386cf5deb727345f1549a9
null096 <33376471+null096@users.noreply.github.com>
2018-05-25 08:08:09 +0300

    Initial commit

---
+++ data/htdocs/phpmyadmin/config.inc.php
@@ -0,0 +1,29 @@
+<?php
+ 
+/* Servers configuration */
+$i = 0;
+ 
+/* Server: localhost [1] */
+$i++;
+$cfg['Servers'][$i]['verbose'] = '';
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['port'] = '';
+$cfg['Servers'][$i]['socket'] = '';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = '124';
+$cfg['Servers'][$i]['nopassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+ 
+/* End of servers configuration */
+ 
+$cfg['blowfish_secret'] = 'kjLGJ8g;Hj3mlHy+Gd~FE3mN{gIATs^1lX+T=KVYv{ubK*U0V';
+$cfg['DefaultLang'] = 'ru';
+$cfg['SendErrorReports'] = 'never';
+$cfg['ServerDefault'] = 1;
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
+ 
+?>
